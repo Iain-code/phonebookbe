ccc2ba0b48c93c0edb00a2bdd327e40a50c26a66
iain-code <iainvirgo1010@live.co.uk>
2025-04-29 18:48:46 +0300

    push everything up

---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require("express")
+const logger = require('morgan');
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(logger('tiny'));
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+const now = new Date()
+app.use(express.json())
+
+app.get("/", (request, response) => {
+    response.json(persons)
+})
+
+app.get("/info", (request, response) => {
+    const content = `Phonebook has info for ${persons.length} people\n${now}`
+    response.type("text/plain")  
+    response.send(content)
+})
+
+app.get("/api/persons/:id", (request, response) => {
+    const foundPerson = persons.find(person => person.id === request.params.id)
+    const body = request.body
+    console.log(request.params.id)
+
+    if (!foundPerson) {
+        response.status(400).json({
+            error: "content missing"
+        })
+    } else {
+    response.json(foundPerson)
+    }
+})
+
+app.delete("/api/persons/:id", (request, response) => {
+    const id = request.params.id
+    const originalLength = persons.length;
+    persons = persons.filter((person) => person.id.toString() !== id)
+    
+    if (persons.length < originalLength) {
+        response.status(204).end(); // Successfully deleted
+    } else {
+        response.status(404).json({error: "Person not found"}); // No person found with that ID
+    }
+})
+
+app.post("/api/persons", (request, response) => {
+    
+    const body = request.body
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: "no content found"
+        })
+    }
+    const found = persons.some(person => person.name === body.name)
+    if (found) {
+        return response.status(409).json({
+            error: "user already exists"
+        })
+    }
+    const generateId = () => {
+        const id = Math.floor(Math.random() * 1000000)
+        return id
+    }
+    const addedPerson = {
+        id: generateId().toString(),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(addedPerson);
+    response.status(201).json(addedPerson);
+});
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
